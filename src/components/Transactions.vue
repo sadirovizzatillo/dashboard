92e4a07b23a96c8e93482116896717d2e1d9c404
Sadirov Izzatillo <sadirovizzatillo3@gmail.com>
2022-07-24 15:29:54 +0500

    last commit

--- src/components/Transactions.vue
+++ src/components/Transactions.vue
@@ -9,13 +9,13 @@
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                 </svg>
-                <img class="w-10 stroke-cyan-500" src="../assets/prew.svg" alt="" />
+                <span class="w-10 stroke-cyan-500" alt=""></span>
             </div>
         </div>
         <p class="text-slate-300 text-xl pt-4 font-medium">Man mar1</p>
         <div class="flex justify-between py-5">
             <div class="flex gap-4">
-                <img class="w-10 stroke-cyan-500" src="../assets/prew.svg" alt="" />
+                <span class="w-10 stroke-cyan-500" alt=""></span>
                 <p class="text-slate-800 text-xl font-medium">
                     Payment to Sent Shakir
                 </p>
@@ -24,7 +24,7 @@
         </div>
         <div class="flex justify-between py-5">
             <div class="flex gap-4">
-                <img class="w-10 stroke-cyan-500" src="../assets/prew.svg" alt="" />
+                <span class="w-10 stroke-cyan-500" alt=""></span>
                 <p class="text-slate-800 text-xl font-medium">
                     Payment to Sent Shakir
                 </p>
@@ -33,7 +33,7 @@
         </div>
         <div class="flex justify-between py-5">
             <div class="flex gap-4">
-                <img class="w-10 stroke-cyan-500" src="../assets/prew.svg" alt="" />
+                <span class="w-10 stroke-cyan-500" alt=""></span>
                 <p class="text-slate-800 text-xl font-medium">
                     Payment to Sent Shakir
                 </p>
